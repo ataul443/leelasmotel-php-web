ea24fec22851fc0c1d554e966fb31ac2c9a6e646
Shubhra <shubhra1028@gmail.com>
2018-04-07 01:20:31 +0530

    MotelAdmin: changed new booking

--- public/js/home.js
+++ public/js/home.js
@@ -19,47 +19,60 @@ var rev = $('#reviews>div');
 
         var roomfull = $('#roomFull');
         var roomdiv = $("#rooms>div");
-        var roomfullimg =  ["url(https://i.imgur.com/OvRQPrw.jpg)", "url(http://www.dhdesigner.net/wp-content/uploads/2017/05/chic-wall-lamps-for-bedroom-wall-lamp-for-bedroom.jpg)", "url(https://vignette.wikia.nocookie.net/yakuza-mob-roleplay/images/9/93/Hotel-Room-With-A-View.jpg/revision/latest?cb=20141121164436)", "url(https://media.timeout.com/images/103639807/image.jpg)"];
-        // var roomfullprice = ["$350", "$450", "$750", "$150"];
+        
+        var roomfullimgStandard =  ["url('http://localhost/motel/leelasmotel-php-web/public/images/standardRoom/01.jpg')", "url('http://localhost/motel/leelasmotel-php-web/public/images/standardRoom/02.jpg')", "url('http://localhost/motel/leelasmotel-php-web/public/images/standardRoom/03.jpg')"];
+        var roomfullpriceStandard = ["$350", "$450", "$750"];
+        var roomfullimgDelux =  ["url('http://localhost/motel/leelasmotel-php-web/public/images/deluxRoom/01.jpg')", "url('http://localhost/motel/leelasmotel-php-web/public/images/deluxRoom/02.jpg')", "url('http://localhost/motel/leelasmotel-php-web/public/images/deluxRoom/03.jpg')"];        
+        var roomfullpriceDelux = ["$550", "$850", "$2750"];
+        var roomfullimgRoyal =  ["url('http://localhost/motel/leelasmotel-php-web/public/images/royalRoom/01.jpg')", "url('http://localhost/motel/leelasmotel-php-web/public/images/royalRoom/02.jpg')", "url('http://localhost/motel/leelasmotel-php-web/public/images/royalRoom/03.jpg')"];
+        var roomfullpriceRoyal = ["$3530", "$2450", "$7550"];
         var roomDet = $('#rooms #roomType');
         var roomPrice = $('#rooms #price');
         var bgimg;
+        var counter = 0;
 
         $('#cross0').click(function () {
             roomfull.hide(500);
         });
         roomdiv.click(function(){
-            roomfull.show(500);
+            roomfull.show(300);
             bgimg = $(this).index();
             carousel0();
         });
         $('#prev').click(function () {
-            bgimg--;
+            counter--;
             carousel0();
         });
         $('#next').click(function () {
-            bgimg++;
+            counter++;
             carousel0();
         });
 
         function carousel0()
         {
-            if(bgimg == roomdiv.length)
-                bgimg=0;
-            if(bgimg<0)
-                bgimg = roomdiv.length-1;
-            roomfull.css({
-                'background-image': roomfullimg[bgimg]
-            });
-            $("#typeDet").text(roomDet[bgimg].innerText);
+            if(counter > 2)
+                counter=0;
+            if(counter<0)
+                counter = 2;
 
-            $("#typePrice").text(roomfullprice[bgimg]);
-            $("#typePrice").text(roomPrice[bgimg].innerText);
-
-
-            /* $("#typePrice").text(roomfullprice[bgimg]); */
-            
-
+            if(bgimg == 0){
+                roomfull.css({
+                    'background-image': roomfullimgStandard[counter]
+                }); 
+                $("#typePrice").text(roomfullpriceStandard[counter]); 
+            } 
+            if(bgimg == 1){
+                roomfull.css({
+                    'background-image': roomfullimgDelux[counter]
+                }); 
+                $("#typePrice").text(roomfullpriceDelux[counter]); 
+            } 
+            if(bgimg == 2){
+                roomfull.css({
+                    'background-image': roomfullimgRoyal[counter]
+                }); 
+                $("#typePrice").text(roomfullpriceRoyal[counter]); 
+            } 
         }
 
         $("#linkAccomo").click(function () { 
@@ -76,7 +89,7 @@ var rev = $('#reviews>div');
             var flag = check();
             if(flag){
                 $("#checkAvailInfoBox").slideDown(400);
-                $("#checkbtn").prop("disabled", true);
+                $("#checkbtn").css("display" , "none");
                 $("#checkInCheck").prop({'value':'Arrival Date' , "disabled": true});
                 $("#checkOutCheck").prop({'value':'Departure Date' , "disabled": true});
                 $("#resetbtn").show(100);
@@ -88,8 +101,8 @@ var rev = $('#reviews>div');
         });
 
         $("#resetbtn").click(function(){
-            $("#checkbtn").prop("disabled", false);            
-            $("#checkInCheck").prop("disabled", false);            
+            $("#checkbtn").prop("disabled", false);
+            $("#checkbtn").css("display" , "block");            
             $("#checkOutCheck").prop("disabled", false);
             $("#resetbtn").hide(100);
             $("#checkAvailInfoBox").slideUp(400);
